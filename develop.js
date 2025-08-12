99e0ab04a41d4c97d2557947bcf79ae59a2655cc
seongjin <kssj9056@gmail.com>
2025-08-12 18:07:42 +0900

    test22

--- develop.js
+++ develop.js
@@ -1,8 +1,6 @@
-const new = "HEllo";
 const express = require("express");
 const app = express();
 const port = 3000;
-
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -10,5 +8,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-const new = "HEllo";
